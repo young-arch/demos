e0cd642fe3e74bcbe402a320356438724019d4a2
Sakyi Gabriel <mawulegabriel@gmail.com>
2023-03-12 02:07:03 +0000

    Changes

--- coin.py
+++ coin.py
@@ -4,9 +4,9 @@ def main():
     mcoin = coind.Coin()
     print("This is the side up: ",mcoin.show_side())
     
-    print("I am about to toss the coin ten times")
+    print("I am about to toss the coin four times")
     
-    for c in range(10):
+    for c in range(4):
         mcoin.toss()
         print(mcoin.show_side())
     
